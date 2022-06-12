0baf83dccda18f8773af95d0c27295ff4b087c1c
Anton Demeshkevich <demeshkevichanton@gmail.com>
2022-06-12 22:44:48 +0300

    refact

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import styled, {createGlobalStyle} from "styled-components";
 
+// const Global = createGlobalStyle`
+// *{
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+//   background-color: aqua;
+// }
+// `
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <>
+      {/*<Global/>*/}
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function

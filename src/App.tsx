dcc018f00ff8203254a8c2519a790f48480d3cc6
Anton Demeshkevich <demeshkevichanton@gmail.com>
2022-06-13 17:54:58 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 import only from "./img/ONLY.png"
-import {Login} from "./Login";
+import {Login} from "./pages/Login";
+// @ts-ignore
+import {Route, Switch} from "react-router-dom";
+import {Profile} from "./pages/Profile";
 // import styled from "styled-components";
 
 // const ImgStyled = styled.div`
@@ -11,16 +14,28 @@ import {Login} from "./Login";
 //   margin-left: 44%
 // `
 
+export const PATH = {
+    LOGIN: "/login",
+    PROFILE: "/profile",
+}
+
 function App() {
     return (
         <div className="App">
+
             {/*<ImgStyled>*/}
-                <img className="img" src={only} alt="only"/>
+            <img className="img" src={only} alt="only"/>
             {/*</ImgStyled>*/}
-
-
-
             <Login/>
+            {/*<Profile/>*/}
+            {/*<Switch>*/}
+            {/*    <Route path={PATH.LOGIN}>*/}
+            {/*        <Login/>*/}
+            {/*    </Route>*/}
+            {/*    <Route path={PATH.PROFILE}>*/}
+            {/*        <Profile/>*/}
+            {/*    </Route>*/}
+            {/*</Switch>*/}
         </div>
     );
 }

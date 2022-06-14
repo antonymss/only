57462302002342c291b1e0a4aa8ca528f5817783
Anton Demeshkevich <demeshkevichanton@gmail.com>
2022-06-14 13:11:12 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import React from 'react';
-import './App.css';
-import only from "./img/ONLY.png"
-import {Login} from "./pages/Login";
-// @ts-ignore
-import {Route, Switch} from "react-router-dom";
-import {Profile} from "./pages/Profile";
-// import styled from "styled-components";
+import { Route, Routes } from 'react-router-dom';
 
-// const ImgStyled = styled.div`
-//   max-height: 78px;
-//   max-width: 180px;
-//   margin-top: 40px;
-//   margin-left: 44%
-// `
+import 'styles/App.css';
+import only from 'assets/img/ONLY.png';
+import { PATH } from 'enums';
+import { Login, Profile } from 'pages';
+import { ReturnComponentType } from 'types';
 
-export const PATH = {
-    LOGIN: "/login",
-    PROFILE: "/profile",
-}
-
-function App() {
-    return (
-        <div className="App">
-
-            {/*<ImgStyled>*/}
-            <img className="img" src={only} alt="only"/>
-            {/*</ImgStyled>*/}
-            <Login/>
-            {/*<Profile/>*/}
-            {/*<Switch>*/}
-            {/*    <Route path={PATH.LOGIN}>*/}
-            {/*        <Login/>*/}
-            {/*    </Route>*/}
-            {/*    <Route path={PATH.PROFILE}>*/}
-            {/*        <Profile/>*/}
-            {/*    </Route>*/}
-            {/*</Switch>*/}
-        </div>
-    );
-}
+const App = (): ReturnComponentType => (
+  <div className="App">
+    <img className="img" src={only} alt="only" />
+    <Routes>
+      <Route path={PATH.LOGIN} element={<Login />} />
+      <Route path={PATH.PROFILE} element={<Profile />} />
+    </Routes>
+  </div>
+);
 
 export default App;
